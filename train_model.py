542e9a43be127eb055ee229438d3447048645db0
B.Praneeth <24n31a0203@mrcet.ac.in>
2025-08-31 17:11:52 +0530

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,5 @@
+# This file is a placeholder for more advanced model training
+# For this basic implementation, we're using a simple TF-IDF approach
+
+print("Basic chatbot doesn't require additional training.")
+print("For more advanced models, you could integrate transformer-based models here.")
